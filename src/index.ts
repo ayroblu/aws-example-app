8b2842e2a04cb7c1bb5d25668f7a8c589a4d931e
Ben Lu <ayroblu@gmail.com>
2020-06-18 20:30:44 +0100

    Get parameter with aws

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
 // TODO
+
+import SSM from "aws-sdk/clients/ssm";
+
+var ssm = new SSM({ region: "eu-west-2" });
+
+export async function getParameter(
+  name: string
+): Promise<SSM.GetParameterResult> {
+  var params = {
+    Name: name,
+    WithDecryption: true,
+  };
+  return new Promise((resolve, reject) =>
+    ssm.getParameter(params, (err, data) => (err ? reject(err) : resolve(data)))
+  );
+}
+async function run() {
+  console.log(await getParameter("SECRET_KEY"));
+  console.log(await getParameter("EMPTY_KEY"));
+}
+run().catch(console.error);
